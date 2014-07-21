732bdb231fe3218f6ba1571989f907a269d99ed3
andra-georgescu <andra.sabina.georgescu@gmail.com>
2014-07-22 01:14:46 +0300

    updated api url

--- app/src/main/java/com/andra/weather/android/WeatherConfig.java
+++ app/src/main/java/com/andra/weather/android/WeatherConfig.java
@@ -2,6 +2,6 @@ package com.andra.weather.android;
 
 public class WeatherConfig {
 
-    public static final String API_BASE_URL = "api.worldweatheronline.com/premium/v1";
+    public static final String API_BASE_URL = "http://api.worldweatheronline.com/premium/v1";
     public static final String API_KEY = "7b38eb333252a70636f3edd76fde9ca5b63da87b";
 }
