c20a48e96e579c805ee0116fb6fcdc9679b5a409
andra-georgescu <andra.sabina.georgescu@gmail.com>
2014-07-22 01:06:46 +0300

    added config file

---
+++ app/src/main/java/com/andra/weather/android/WeatherConfig.java
@@ -0,0 +1,7 @@
+package com.andra.weather.android;
+
+public class WeatherConfig {
+
+    public static final String API_BASE_URL = "api.worldweatheronline.com/premium/v1";
+    public static final String API_KEY = "7b38eb333252a70636f3edd76fde9ca5b63da87b";
+}
